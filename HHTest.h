e4cf354a632dcc04592089da0e0205959755e4f2
Harrison Hertzman <harrison@Harrisons-MacBook-Pro.local>
2013-10-22 13:17:03 -0400

    added a test class

---
+++ HHTest.h
@@ -0,0 +1,13 @@
+//
+//  HHTest.h
+//  Git Test
+//
+//  Created by Harrison Hertzman on 10/22/13.
+//  Copyright (c) 2013 Harrison Hertzman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHTest : NSObject
+
+@end
---
+++ HHTest.m
@@ -0,0 +1,13 @@
+//
+//  HHTest.m
+//  Git Test
+//
+//  Created by Harrison Hertzman on 10/22/13.
+//  Copyright (c) 2013 Harrison Hertzman. All rights reserved.
+//
+
+#import "HHTest.h"
+
+@implementation HHTest
+
+@end
